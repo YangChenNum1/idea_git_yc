04678792bcbe4f617d5f3255d71b02200abd04ee
YangChenNum1 <2071364112@qq.com>
2018-07-02 20:31:39 +0800

    第一次提交，项目的基础结构和基础代码

---
+++ app-dao/src/main/java/com/baizhi/dao/EmployeeDao.java
@@ -0,0 +1,12 @@
+package com.baizhi.dao;
+
+import com.baizhi.entity.Employee;
+
+/**
+ * Created by HP on 2018/7/1.
+ */
+public class EmployeeDao {
+    public void LoginEmployee(Employee emp){
+        System.out.println(emp);
+    }
+}
